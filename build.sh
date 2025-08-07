eb9bddfea9883f67366cfd027a106137e4a71662
natanieljoshe <natanieljoshe@gmail.com>
2025-08-07 18:48:49 +0700

    fix build sh

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Install Composer dependencies
 composer install --no-dev --no-interaction --optimize-autoloader
 
-# Run database migrations FIRST
-# Migrations will read the environment variables directly
-php artisan migrate --force
-
-# Clear and cache the configuration LAST
-# This caches the correct production database config for the running app
-php artisan optimize:clear
 php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan migrate --force
